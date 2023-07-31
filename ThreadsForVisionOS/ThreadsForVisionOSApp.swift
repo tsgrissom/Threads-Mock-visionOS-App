fd75128fec765aad70208b5343130b17385299fc
T0xicTyler <grisstyl@gmail.com>
2023-07-30 21:58:27 -0500

    Initial Commit

---
+++ ThreadsForVisionOS/ThreadsForVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsForVisionOSApp.swift
+//  ThreadsForVisionOS
+//
+//  Created by Tyler Grissom on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsForVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
